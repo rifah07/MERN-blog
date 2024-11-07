778d12cc4c8638379c01e3fb55a9d7b9601c5199
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-11-07 09:10:00 +0600

    Article Contents added

--- client/src/pages/Article.jsx
+++ client/src/pages/Article.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import articles from "./ArticleContent";
 
 const Article = () => {
-  const {name}= useParams();
+  const { name } = useParams();
+  const article = articles.find((article) => article.name === name);
+  if(!article) return <h1>Article does not exists in this blog</h1>;
   return (
     <div>
       <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
-        This is the {name} Article
+        {article.title}
       </h1>
+      {article.content.map((paragraph, index) => (
+        <p className="mx-auto leading-relaxed text-base mb-4" key={index}>
+          {paragraph}
+        </p>
+      ))}
     </div>
   );
 };
