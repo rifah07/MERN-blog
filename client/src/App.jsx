b2272796ea7f08f06d4137041221e7a423692b69
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-11-07 07:44:43 +0600

    blog pages created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
+import About from "./pages/About";
+import ArticleList from "./pages/ArticleList";
+import Article from "./pages/Article";
+
 import "./App.css";
 function App() {
   return (
@@ -8,7 +12,10 @@ function App() {
       {" "}
       <div className="max-w-screen-md mx-auto pt-20">
         <Routes>
-        <Route path='/' element={<Home/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articles-list" element={<ArticleList />} />
+          <Route path="/article" element={<Article />} />
         </Routes>
       </div>
     </Router>
---
+++ client/src/pages/About.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
+        About
+      </h1>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit,
+        praesentium temporibus pariatur itaque illo accusamus ex error nemo
+        similique sit deserunt vero odio labore, accusantium aliquam dolorem
+        tempore. Ut debitis dolor laudantium aliquam ea, sit aperiam quod
+        voluptatibus harum natus voluptatum laboriosam, molestiae molestias
+        incidunt eum fuga nostrum nulla quos!
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Impedit, praesentium temporibus pariatur itaque illo accusamus ex error
+        nemo similique sit deserunt vero odio labore, accusantium aliquam
+        dolorem tempore. Ut debitis dolor laudantium aliquam ea, sit aperiam
+        quod voluptatibus harum natus voluptatum laboriosam, molestiae molestias
+        incidunt eum fuga nostrum nulla quos!
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, nam
+        accusamus iste quibusdam soluta blanditiis in asperiores. Dolor aperiam
+        distinctio officia numquam pariatur, doloremque, harum ipsam earum
+        dignissimos, praesentium quibusdam maiores iusto amet accusantium
+        inventore! Minus id officiis doloribus, voluptatibus dicta molestiae. Ea
+        eos a aperiam exercitationem eaque accusamus tempora quo quibusdam
+        voluptatum odit, in officia consequuntur vel tenetur placeat minima. Qui
+        eos velit veniam officiis vel illo minus corrupti ad, consectetur enim
+        porro aliquid temporibus explicabo nulla et, repellendus expedita! Modi
+        ratione dolor quibusdam dicta autem, culpa odio. Quae asperiores vitae
+        hic rem quidem tempora veritatis possimus quas fugiat?
+      </p>
+    </div>
+  );
+};
+
+export default About;
