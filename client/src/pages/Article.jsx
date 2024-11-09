e3e62f63e54c846984a872e8631e71307511e685
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-11-09 10:28:59 +0600

    list modifies at each blog page

--- client/src/pages/Article.jsx
+++ client/src/pages/Article.jsx
@@ -7,6 +7,7 @@ const Article = () => {
   const { name } = useParams();
   const article = articleContent.find((article) => article.name === name);
   if (!article) return <h1>Article does not exists in this blog</h1>;
+  const otherArticles= articleContent.filter(article => article.name !== name);
   return (
     <>
       <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
@@ -21,7 +22,7 @@ const Article = () => {
         Other Articles
       </h1>
       <div className="flex flex-wrap -m-4">
-        <Articles articles={articleContent} />
+        <Articles articles={otherArticles} />
       </div>
     </>
   );
