9874bd5518562bf1eef094c3d9fa6e9518dee19f
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-11-09 10:22:01 +0600

    At each blog page, article list added

--- client/src/pages/Article.jsx
+++ client/src/pages/Article.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import articles from "./ArticleContent";
+import articleContent from "./ArticleContent";
+import Articles from "../components/Articles";
 
 const Article = () => {
   const { name } = useParams();
-  const article = articles.find((article) => article.name === name);
-  if(!article) return <h1>Article does not exists in this blog</h1>;
+  const article = articleContent.find((article) => article.name === name);
+  if (!article) return <h1>Article does not exists in this blog</h1>;
   return (
-    <div>
+    <>
       <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
         {article.title}
       </h1>
@@ -16,7 +17,13 @@ const Article = () => {
           {paragraph}
         </p>
       ))}
-    </div>
+      <h1 className="sm:text-2xl text-xl font-bold my-4 text-gray-900">
+        Other Articles
+      </h1>
+      <div className="flex flex-wrap -m-4">
+        <Articles articles={articleContent} />
+      </div>
+    </>
   );
 };
 
