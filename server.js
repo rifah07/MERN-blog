1f5ef79bce4c90a71774bc59cccd3443eae89c1f
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-11-10 19:50:21 +0600

    Connection checked with postman by post and get operation

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express=require('express')
 const app= express();
 const PORT= process.env.PORT || 3000
 
+app.use(express.json({extended:false}));
+
 app.get('/', (req, res) => res.send("Assalamu alaikum!"));
+app.post("/", (req, res) => res.send(`Assalamu alaikum ${req.body.name}`));
 
 app.listen(PORT, ()=> console.log(`Server started at port ${PORT}`));
