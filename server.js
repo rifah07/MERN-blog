3084ba1266934c4618fda74b3c574c0ebd4f5e48
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-11-10 20:38:48 +0600

    Route params tried and comment added

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express=require('express')
 const app= express();
 const PORT= process.env.PORT || 3000
 
+//built in middleware
 app.use(express.json({extended:false}));
 
+//route to check connection
 app.get('/', (req, res) => res.send("Assalamu alaikum!"));
 app.post("/", (req, res) => res.send(`Assalamu alaikum ${req.body.name}`));
+
+//route parameter
+app.get('/greet/:name', (req, res) => res.send(`Assalamu alaikum ${req.params.name}!`));
 
 app.listen(PORT, ()=> console.log(`Server started at port ${PORT}`));
