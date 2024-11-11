a820edb2b9cc53634d8926c6a312caf4f0275aa1
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-11-11 19:21:39 +0600

    comment functionality imolemented without database

--- server.js
+++ server.js
@@ -13,10 +13,17 @@ const articlesInfo= {
     "my-thoughts-on-learning-react" : {
         comments: [],
     },
-}
+};
 
 //built in middleware
 app.use(express.json({extended:false}));
+
+app.post("/api/articles/:name/add-comments", (req,res) => {
+    const {username, text} = req.body;
+    const articleName = req.params.name;
+    articlesInfo[articleName].comments.push({username, text});
+    res.status(200).send(articlesInfo[articleName]);
+});
 
 //route to check connection
 //app.get('/', (req, res) => res.send("Assalamu alaikum!"));
