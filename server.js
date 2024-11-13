051f57d3db95550901b33591ed5fac4056a45319
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-11-13 20:27:59 +0600

    data  added and fetched from mongodb database through express js

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const app = express();
-const {MongoClient}= require("mongodb");
+const { MongoClient } = require("mongodb");
 const PORT = process.env.PORT || 3000;
 
 //built in middleware
 app.use(express.json({ extended: false }));
 
-app.get('api/articles/:name', (req, res) => {
+app.get("/api/articles/:name", async (req, res) => {
+  try {
     const articleName = req.params.name;
-} )
+    const client = await MongoClient.connect("mongodb://localhost:27017");
+    const db = client.db("mernblog");
+    const articlesInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    res.status(200).json(articlesInfo);
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "ERror connecting to Database", error });
+  }
+});
 
 app.post("/api/articles/:name/add-comments", (req, res) => {
   const { username, text } = req.body;
