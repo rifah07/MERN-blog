bacda58d36c2398bd8ad88f6b4379ef1b09c65f7
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-11-13 19:47:59 +0600

    mongodb required

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const {MongoClient}= require("mongodb");
 const PORT = process.env.PORT || 3000;
 
 //built in middleware
 app.use(express.json({ extended: false }));
+
+app.get('api/articles/:name', (req, res) => {
+    const articleName = req.params.name;
+} )
 
 app.post("/api/articles/:name/add-comments", (req, res) => {
   const { username, text } = req.body;
