9f5e2b8eb16071502a3f24d3a5c29953d8967f0e
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-11-16 10:37:39 +0600

    comments part added and styled

--- client/src/components/CommentsList.jsx
+++ client/src/components/CommentsList.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const CommentsList = ({comments}) => {
-  return <div>CommentsList</div>;
+const CommentsList = ({ comments }) => {
+  return (
+    <>
+      <h3 className="sm:text-2xl text-xl font-bold my-6 text-gray-900">
+        Comments:
+      </h3>
+    </>
+  );
 };
 
 export default CommentsList;
