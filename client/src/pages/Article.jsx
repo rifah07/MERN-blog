5fb70ca202a83a78a7f8812a18102a7d9ec559a7
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-11-16 10:26:36 +0600

    comments are loaded from database to console

--- client/src/pages/Article.jsx
+++ client/src/pages/Article.jsx
@@ -11,8 +11,15 @@ const Article = () => {
   const article = articleContent.find((article) => article.name === name);
   const [articleInfo, setArticleInfo] = useState({ comments: [] });
   useEffect(() => {
-    console.log("Component mounted");
-  });
+    const fetchData= async ()=>{
+      const result= await fetch(`/api/articles/${name}`);
+      const body= await result.json();
+      console.log(body);
+      setArticleInfo(body);
+    }; 
+    fetchData();
+  }, [name]);
+
   if (!article) return <NotFound />;
   const otherArticles = articleContent.filter(
     (article) => article.name !== name
