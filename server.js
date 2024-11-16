511722e5e1dfdbe5f06b3f1521663708d595fe8b
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-11-16 10:16:03 +0600

    both frontend and backend are running by "npm run dev"

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const { MongoClient } = require("mongodb");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 //built in middleware
 app.use(express.json({ extended: false }));
